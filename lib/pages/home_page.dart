f562e935daa05543301bca931c6194c80e1979ee
KeingCasabona <keing.limache.casabona@gmail.com>
2024-11-15 23:45:48 -0500

    feat: Add inicializando el mapa con nuestra ubicacion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<CameraPosition> initCurretLocation() async {
+    Position currentPosition = await Geolocator.getCurrentPosition();
+    return CameraPosition(
+        target: LatLng(currentPosition.latitude, currentPosition.longitude),
+        zoom: 15);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,13 +23,18 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('My Places'),
       ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(-12.133306, -76.983687),
-          zoom: 15,
-        ),
-        myLocationEnabled: true,
-      ),
+      body: FutureBuilder(
+          future: initCurretLocation(),
+          builder: (BuildContext context, AsyncSnapshot snap) {
+            if (snap.hasData) {
+              return GoogleMap(
+                initialCameraPosition: snap.data,
+                myLocationEnabled: true,
+                //myLocationButtonEnabled: true,
+              );
+            }
+            return const Center(child: CircularProgressIndicator());
+          }),
     );
   }
 }
