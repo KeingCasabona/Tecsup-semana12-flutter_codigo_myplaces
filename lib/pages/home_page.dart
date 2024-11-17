c98415fb007a889506358fc6835c61c4ae102d1d
KeingCasabona <keing.limache.casabona@gmail.com>
2024-11-16 20:25:59 -0500

    feat: Add moviendo camara a nuestra ubicacion

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:myplaces/utils/map_style.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,11 +13,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late GoogleMapController googleMapController;
+
   Future<CameraPosition> initCurretLocation() async {
     Position currentPosition = await Geolocator.getCurrentPosition();
     return CameraPosition(
         target: LatLng(currentPosition.latitude, currentPosition.longitude),
         zoom: 15);
+  }
+
+  Future<void> moveCamera() async {
+    Position currentPosition = await Geolocator.getCurrentPosition();
+    CameraUpdate cameraUpdate = CameraUpdate.newLatLng(
+      LatLng(currentPosition.latitude, currentPosition.longitude),
+    );
+    googleMapController.animateCamera(cameraUpdate);
   }
 
   @override
@@ -27,10 +40,41 @@ class _HomePageState extends State<HomePage> {
           future: initCurretLocation(),
           builder: (BuildContext context, AsyncSnapshot snap) {
             if (snap.hasData) {
-              return GoogleMap(
-                initialCameraPosition: snap.data,
-                myLocationEnabled: true,
-                //myLocationButtonEnabled: true,
+              return Stack(
+                children: [
+                  GoogleMap(
+                    initialCameraPosition: snap.data,
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: false,
+                    onMapCreated: (GoogleMapController controller) {
+                      googleMapController = controller;
+                      googleMapController.setMapStyle(jsonEncode(mapStyle));
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          moveCamera();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueAccent,
+                        ),
+                        child: Text(
+                          'Mi ubicacion',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               );
             }
             return const Center(child: CircularProgressIndicator());
